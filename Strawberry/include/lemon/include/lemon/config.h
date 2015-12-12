44092c273b050f627f91a6390e56a88fb6f326e8
Ruolin Liu <rliu0606@gmail.com>
2015-12-12 09:48:13 -0600

    add Eigen and Lemon headers

---
+++ Strawberry/include/lemon/include/lemon/config.h
@@ -0,0 +1,27 @@
+/* lemon/config.h.  Generated from config.h.in by configure.  */
+/* The version string */
+#define LEMON_VERSION 1.3.1
+
+/* Define to 1 if you have long long */
+#define LEMON_HAVE_LONG_LONG 1
+
+/* Define to 1 if you have any LP solver. */
+#define LEMON_HAVE_LP 1
+
+/* Define to 1 if you have any MIP solver. */
+#define LEMON_HAVE_MIP 1
+
+/* Define to 1 if you have CPLEX. */
+/* #undef LEMON_HAVE_CPLEX */
+
+/* Define to 1 if you have GLPK. */
+#define LEMON_HAVE_GLPK 1
+
+/* Define to 1 if you have SOPLEX */
+/* #undef LEMON_HAVE_SOPLEX */
+
+/* Define to 1 if you have CLP */
+/* #undef LEMON_HAVE_CLP */
+
+/* Define to 1 if you have CBC */
+/* #undef LEMON_HAVE_CBC */
