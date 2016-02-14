280d2f9771cf4902d96333c245c7a7ec0dcf63bd
Ruolin Liu <rliu0606@gmail.com>
2016-02-14 15:10:59 -0600

    Strawberry 0.7.2 version

---
+++ include/version.hpp
@@ -0,0 +1,21 @@
+/*
+ * version.hpp
+ *
+ *  Created on: Jan 12, 2016
+ *      Author: Ruolin Liu
+ */
+
+#ifndef VERSION_HPP_
+#define VERSION_HPP_
+
+#include <string>
+namespace strawberry{
+   class VersionChecker{
+   public:
+      std::string message();
+   };
+}
+
+
+
+#endif /* VERSION_HPP_ */
