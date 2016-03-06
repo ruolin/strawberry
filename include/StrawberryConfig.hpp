1202cb0e19ea73aee27f31e438a732d93a61a660
Ruolin Liu <rliu0606@gmail.com>
2016-03-06 09:50:54 -0600

    v0.7.3 Valgrind test pass

--- include/StrawberryConfig.hpp
+++ include/StrawberryConfig.hpp
@@ -11,8 +11,8 @@
 namespace strawberry{
    constexpr char majorVersion[] = "0";
    constexpr char minorVersion[] = "7";
-   constexpr char patchVersion[] = "2";
-   constexpr char version[] = "0.7.2";
+   constexpr char patchVersion[] = "3";
+   constexpr char version[] = "0.7.3";
 }
 
 
