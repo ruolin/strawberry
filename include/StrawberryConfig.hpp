221160c99c01a61f5b9ab5c146802a6f15821797
ruolin <rliu0606@gmail.com>
2019-05-18 10:28:38 -0700

    update version

--- include/StrawberryConfig.hpp
+++ include/StrawberryConfig.hpp
@@ -11,8 +11,8 @@
 namespace strawberry{
    constexpr char majorVersion[] = "1";
    constexpr char minorVersion[] = "1";
-   constexpr char patchVersion[] = "0";
-   constexpr char version[] = "1.1.0";
+   constexpr char patchVersion[] = "1";
+   constexpr char version[] = "1.1.1";
 }
 
 
